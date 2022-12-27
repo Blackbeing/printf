9f4ce832c2b252b35dc4de7a8cd3a93501c1cba7
Blackbeing <johnjahi55@gmail.com>
2022-12-27 15:41:50 +0000

    refactor !Fixup

--- my_printf.c
+++ my_printf.c
@@ -12,15 +12,11 @@ int _printf(const char *format, ...)
 	unsigned int fmt_counter = 0, ops_counter = 0, bytes = 0;
 	va_list args;
 
-	c_l ops[] = {
-		{"c", print_char},
-		{"s", print_str},
-		{"%", print_perc},
+	c_l ops[] = { {"c", print_char}, {"s", print_str}, {"%", print_perc},
 		{NULL, NULL},
 	};
 	if (format == NULL)
 		return (-1);
-
 	va_start(args, format);
 	while (format && format[fmt_counter])
 	{
@@ -33,13 +29,18 @@ int _printf(const char *format, ...)
 			while (ops_counter < 3)
 			{
 				if (format[fmt_counter] == *(ops[ops_counter]).fmt)
+				{
 					bytes += ops[ops_counter].func(args);
+					break;
+				}
 				ops_counter++;
 			}
-			if (ops_counter < 3)
-				fmt_counter++;
-			else
+			if (ops_counter >= 3)
+			{
 				bytes += _write('%');
+				bytes += _write(format[fmt_counter]);
+			}
+			fmt_counter++;
 		}
 		else
 		{
